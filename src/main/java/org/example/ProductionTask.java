10f9b7eb0e52d94e939b6d045582bb6176027803
Juank003 <jortealv@myuax.com>
2023-10-16 13:20:43 +0200

    Compila

--- src/main/java/org/example/ProductionTask.java
+++ src/main/java/org/example/ProductionTask.java
@@ -3,16 +3,19 @@ package org.example;
 import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 
+
 public class ProductionTask implements Runnable {
     private final BlockingQueue<Integer> buffer;
     private final Visualizer visualizer;
     private final int workstationId;
-    private final Random random = new Random();
+    private int[] distribution; // Estructura para rastrear la distribución de componentes
+    private Random random = new Random();
 
     public ProductionTask(int workstationId, BlockingQueue<Integer> buffer, Visualizer visualizer) {
         this.workstationId = workstationId;
         this.buffer = buffer;
         this.visualizer = visualizer;
+        this.distribution = new int[100]; // Inicializa con el número apropiado de contenedores
     }
 
     @Override
@@ -22,11 +25,14 @@ public class ProductionTask implements Runnable {
                 // Simula la producción de componentes
                 int component = produceComponent();
 
+                // Actualiza la estructura de distribución
+                updateDistribution(component);
+
                 // Coloca el componente en el búfer compartido
                 buffer.put(component);
 
-                // Muestra la distribución en tiempo real
-                updateDistribution();
+                // Llama al método para reflejar la distribución en el visualizador
+                visualizer.updateDistribution(distribution);
 
                 // Agrega una pausa para simular la producción
                 Thread.sleep(500); // Ajusta el tiempo según tus necesidades
@@ -41,8 +47,11 @@ public class ProductionTask implements Runnable {
         return random.nextInt(100);
     }
 
-    private void updateDistribution() {
-        // Aquí debes implementar la lógica para actualizar la distribución en el visualizador
-        // Puedes usar visualizer.updateDistribution(currentDistribution) si es necesario
+    private void updateDistribution(int component) {
+        // Implementa la lógica para actualizar la distribución con el nuevo componente
+        // Puede ser necesario calcular en qué contenedor debe colocarse el componente
+        // y aumentar la cantidad en ese contenedor.
+        // Asegúrate de manejar adecuadamente los índices y límites del array.
     }
 }
+
